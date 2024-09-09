576334ff49c2aee3b522ab9d477e07678f5a9028
sdv <sumenkov.dv@yandex.ru>
2024-09-09 15:40:50 +0700

    first commit

---
+++ src/main/java/ru/sumenkov/SiberianSeaBattle/SiberianSeaBattleApplication.java
@@ -0,0 +1,13 @@
+package ru.sumenkov.SiberianSeaBattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiberianSeaBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiberianSeaBattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sumenkov/SiberianSeaBattle/SiberianSeaBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sumenkov.SiberianSeaBattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiberianSeaBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
