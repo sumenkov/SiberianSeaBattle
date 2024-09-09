57cd2fd3e7d4ef2619436cb1ad0f2c5d4275dec1
Andrey Maksimov <onixbed@gmail.com>
2024-09-09 21:21:24 +0700

    Добавлена логика заполения  флота

--- src/main/java/ru/sumenkov/SiberianSeaBattle/SiberianSeaBattleApplication.java
+++ src/main/java/ru/sumenkov/SiberianSeaBattle/SiberianSeaBattleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SiberianSeaBattleApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SiberianSeaBattleApplication.class, args);
 	}
-
 }
---
+++ src/main/java/ru/sumenkov/SiberianSeaBattle/controller/TestController.java
@@ -0,0 +1,49 @@
+/*
+ *  Copyright 2023 Contributors to the Sports-club.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package ru.sumenkov.SiberianSeaBattle.controller;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+import ru.sumenkov.SiberianSeaBattle.model.Fleet;
+import ru.sumenkov.SiberianSeaBattle.model.Warship;
+import ru.sumenkov.SiberianSeaBattle.service.GameService;
+
+/**
+ * Description:
+ *
+ * @author <a href="mailto:onixbed@gmail.com">amaksimov</a>
+ * crested on 09.09.2024
+ */
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+public class TestController {
+    private final GameService gameService;
+
+    @PostConstruct
+    void initTest() {
+        Fleet fleet = gameService.getFleet(10,
+                                           10);
+        log.info("fleat");
+        for (Warship warship : fleet.getWarships()) {
+            log.info("start " + warship.start() + "end " + warship.end());
+        }
+
+    }
+
+}
