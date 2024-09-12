cef044ff266defb79ee72a5ca7cb29de4a5f9b16
Evgeny Kurensky <ekurensky@gmail.com>
2024-09-13 01:41:49 +0700

    рабочий вебсокет

--- src/main/java/ru/sumenkov/SiberianSeaBattle/configuration/SpringSecurityConfiguration.java
+++ src/main/java/ru/sumenkov/SiberianSeaBattle/configuration/SpringSecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
