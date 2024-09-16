d076769b89b6bafc15b13f08e8d272b22b129a04
leonid <leonid@computer.local>
2024-09-16 23:41:08 +0700

    Определение позиции корабля на доске

--- front/index.ts
+++ front/index.ts
@@ -1,17 +1,17 @@
+import Board from "./src/game/Board";
 import GameLoop from "./src/game/GameLoop";
-import drawBoardHook from "./src/game/hooks/drawBoardHook";
 import createCanvas from "./src/game/initializeCanvas";
 import Ship from "./src/game/Ship";
 
-let x = 0;
-let y = 0;
+const canvas = createCanvas({});
 
-const canvas = createCanvas({
-    onMouseMove: (e) => {
-        x = e.offsetX;
-        y = e.offsetY;
+const board = new Board({
+    canvas,
+    position: {
+        x: 100,
+        y: 100
     }
-});
+})
 
 const oneSegmentShip = new Ship({
     initialX: 700,
@@ -30,17 +30,11 @@ const twoSegmentShip = new Ship({
 const loop = new GameLoop(canvas);
 document.body.appendChild(canvas);
 
-loop.use((context, canvas) => {
-    drawBoardHook(context, canvas, {
-        width: 500,
-        height: 500,
-        displayPositionY: 100,
-        displayPositionX: 100,
-        activeY: y,
-        activeX: x
-    })
-});
 
+board.addShip(oneSegmentShip);
+board.addShip(twoSegmentShip);
+
+loop.use(board.draw);
 loop.use(oneSegmentShip.draw.bind(oneSegmentShip));
 loop.use(twoSegmentShip.draw.bind(twoSegmentShip));
 
--- front/src/utils/getSpriteBySegmentAmount.ts
+++ front/src/utils/getSpriteBySegmentAmount.ts
@@ -1,7 +1,22 @@
 import oneSegmentedShip from '../assets/ships/1-segment-ship.png.b64';
-import twoSegmentedShip from '../assets/ships/2-segment-ship.png.b64';
+import oneSegmentedShipVert from '../assets/ships/1-segment-ship-v.png.b64';
 
-function getSpriteBySegmentAmount(segmentAmount: number) {
+import twoSegmentedShip from '../assets/ships/2-segment-ship.png.b64';
+import twoSegmentedShipVert from '../assets/ships/2-segment-ship-v.png.b64';
+
+import { DIRECTIONS } from '../game/Ship';
+
+function getSpriteBySegmentAmount(segmentAmount: number, direction: DIRECTIONS) {
+
+    if (direction === DIRECTIONS.HORIZONTAL) {
+
+        switch (segmentAmount) {
+            case 1: return oneSegmentedShipVert;
+            case 2: return twoSegmentedShipVert;
+            default: return oneSegmentedShipVert;
+        }
+    }
+
     switch (segmentAmount) {
         case 1: return oneSegmentedShip;
         case 2: return twoSegmentedShip;
