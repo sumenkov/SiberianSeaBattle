79a82d8f58cb2ed2d7a94e7080f6c76a2421d472
Andrey Maksimov <onixbed@gmail.com>
2024-09-17 08:55:31 +0700

    Унификация конфигов в единый пакет

--- src/main/java/ru/sumenkov/SiberianSeaBattle/configuration/WebSocketConfiguration.java
+++ src/main/java/ru/sumenkov/SiberianSeaBattle/configuration/WebSocketConfiguration.java
@@ -1,27 +1,24 @@
-package ru.sumenkov.SiberianSeaBattle.config;
-import org.springframework.beans.factory.annotation.Autowired;
+package ru.sumenkov.SiberianSeaBattle.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+        config.setApplicationDestinationPrefixes("/game");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat");
-        registry.addEndpoint("/chat").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*")
+                .withSockJS();
     }
 
 }
