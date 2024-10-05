7905aa983269c502ff8cb3e135c5ddfb0b96422d
leonid <leonid@computer.local>
2024-10-05 14:20:56 +0700

    fix: импорты карнтинок

--- front/src/views/Game/Loading.layout.ts
+++ front/src/views/Game/Loading.layout.ts
@@ -1,11 +1,14 @@
+import pattern from '../../assets/pattern.png.b64';
+import crab from '../../assets/crab.png.b64';
+
 export default (
     '<div class="bg">' +
-    '    <img src="/src/assets/pattern.png" alt="">' +
+    `    <img src="${pattern}" alt="">` +
     '</div>' +
     '<div class="wrapper" style="height: 100%;">' +
     '    <div style="height: 100%; display: flex; align-items: center; justify-content: center; padding: 10px;">' +
     '        <div class="panel waiting-room">' +
-    '            <img class="preloader-spinner" src="/src/assets/crab.png" alt="">' +
+    `            <img class="preloader-spinner" src="${crab}" alt="">` +
     '        </div>' +
     '    </div>' +
     '</div>'
